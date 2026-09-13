422994644b8eed467e3db25fa6da6a37299729dd
XKspeed <xkspeed@users.noreply.github.com>
2026-09-13 11:52:05 +0800

    加dp import

--- app/src/main/java/com/apkeditor/miuix/ui/App.kt
+++ app/src/main/java/com/apkeditor/miuix/ui/App.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.apkeditor.miuix.data.ApkDataService
 import com.apkeditor.miuix.data.RealApkDataService
 import top.yukonga.miuix.kmp.basic.FloatingNavigationBar

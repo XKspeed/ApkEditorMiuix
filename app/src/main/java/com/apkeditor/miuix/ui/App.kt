f0391241ba24eb3b56d6f937cd6a7ae804894ae1
XKspeed <xkspeed@users.noreply.github.com>
2026-09-14 22:14:29 +0800

    添加 padding 的导入

--- app/src/main/java/com/apkeditor/miuix/ui/App.kt
+++ app/src/main/java/com/apkeditor/miuix/ui/App.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState

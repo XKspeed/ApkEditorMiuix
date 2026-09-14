a5115d6ebe2841bb32253777790a514569a2a4f4
XKspeed <xkspeed@users.noreply.github.com>
2026-09-15 06:50:16 +0800

    修复编译错误：添加 WindowInsets.add 和 WindowInsetsSides 的导入

--- app/src/main/java/com/apkeditor/miuix/ui/ThirdPartyLicensesPage.kt
+++ app/src/main/java/com/apkeditor/miuix/ui/ThirdPartyLicensesPage.kt
@@ -4,6 +4,8 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.add
 import androidx.compose.foundation.layout.asPaddingValues
 import androidx.compose.foundation.layout.displayCutout
 import androidx.compose.foundation.layout.fillMaxSize

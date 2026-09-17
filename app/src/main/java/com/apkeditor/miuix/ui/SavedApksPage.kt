f64d1bb00461e5c361bf6f0177cc7c3e99a9902e
XKspeed <3212882324@qq.com>
2026-09-17 12:09:04 +0800

    fix(blur): add missing rememberBlurBackdrop import; drop bogus matchParentSize import

--- app/src/main/java/com/apkeditor/miuix/ui/SavedApksPage.kt
+++ app/src/main/java/com/apkeditor/miuix/ui/SavedApksPage.kt
@@ -43,6 +43,7 @@ import com.apkeditor.miuix.ui.util.BlurredBar
 import com.apkeditor.miuix.ui.util.LocalIsWideScreen
 import com.apkeditor.miuix.ui.util.pageContentPadding
 import com.apkeditor.miuix.ui.util.pageScrollModifiers
+import com.apkeditor.miuix.ui.util.rememberBlurBackdrop
 import top.yukonga.miuix.kmp.basic.Button
 import top.yukonga.miuix.kmp.basic.Card
 import top.yukonga.miuix.kmp.basic.HorizontalDivider
